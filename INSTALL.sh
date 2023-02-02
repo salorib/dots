fd3e2d737cc69830fc9f4be8948d65cdc8e338a6
salorib <santi.li.correo@gmail.com>
2023-02-01 22:27:18 -0600

    Added install fonts zsh yay

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,42 +1,18 @@
 #!/bin/bash
 
-Packages="xdg-user-dirs,picom,zsh,neovim,rofi,polybar,thunar,ranger,python-pywal,feh,flameshot,i3lock,neofetch,bashtop"
+#Packages to install
+Packages="git,xdg-user-dirs,picom,zsh,neovim,rofi,polybar,thunar,ranger,python-pywal,feh,flameshot,i3lock,neofetch,bashtop"
+#Fonts to install
 Fonts="ttf-font-awesome,ttf-jetbrains-mono,nerd-fonts-jetbrains-mono"
 oldIFS=$IFS
 IFS=,
 HOME=/home/salorib
 
-#AUR package manager
-sudo pacman -S base-devel git
-mkdir aur
-cd aur
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-#install fonts
-for Font in $Fonts;
-do
-	checkpckg $Font
-done
-
-#Install zsh and dependencies
-sudo pacman -S zsh
-#Change default shell to zsh:
-chsh -s $(which zsh)
-#Install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#Install powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-#Copy zsh config files to home directory
-cp $HOME/.config/zsh/.zshrc $HOME
-cp $HOME/.config/zsh/.p10k.zsh $HOME
-
-
+#Check if packages are already installed
 checkpckg(){
 echo "Checking if $1 is installed:"
 if pacman -Q $1 > /dev/null; then 
-	echo $1 is installed, skiping;
+	echo $1 is installed, skipping;
 else
 	read -p "$1 is not installed, install $1? (y/n) " var
 	if [[ $var == 'y' || $var == 'Y' || $var == 'yes' || $var == 'Yes' || $var == 'YES' ]]; then 
@@ -46,10 +22,57 @@ fi
 echo '';
 }
 
+
+#AUR package manager
+echo "Checking if yay is installed:"
+if pacman -Q yay > /dev/null ; then 
+	echo yay is installed, skipping;
+	echo ""
+else
+	sudo pacman -S base-devel
+	mkdir aur
+	cd aur
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+fi
+
+#Install fonts
+for Font in $Fonts;
+do
+	checkpckg $Font
+done
+
+#Install all Packages
 for Pack in $Packages;
 do
 	checkpckg $Pack
 done
+
+read -p "Install Oh My Zsh! ? (y/n) " var
+if [[ $var == 'y' || $var == 'Y' || $var == 'yes' || $var == 'Yes' || $var == 'YES' ]]; then 
+	if ! pacman -Q zsh > /dev/null ; then
+		echo "zsh is not installed, install zsh before install Oh My Zsh!"
+		#Install zsh and dependencies
+		sudo pacman -S zsh
+		#Change default shell to zsh:
+		chsh -s $(which zsh)
+	fi
+	#Install oh my zsh
+	echo "Installing oh my zsh! ..."
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	
+	#Install powerlevel10k theme
+	echo Installing powerlevel10k theme ...
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+	#Copy zsh config files to home directory
+	echo Copying configuration ...
+	cp $HOME/.config/zsh/.zshrc $HOME
+	cp $HOME/.config/zsh/.p10k.zsh $HOME
+else
+	echo ""
+
+fi
 
 #Update XDG directories
 if pacman -Q xdg-user-dirs > /dev/null; then
